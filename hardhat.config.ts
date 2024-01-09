7368d25e71fcaaae1316558cf08c911e62516349
eason.web3@gmail.com <eason.web3@gmail.com>
2024-01-09 11:23:39 +0800

    test: version
    test:modidy version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -33,7 +33,7 @@ const OPTIMISM_PRIVATE_KEY =
 
 
   const optimizedComilerSettings = {
-    version: '0.8.17',
+    version: '0.8.4',
     settings: {
       optimizer: { enabled: true, runs: 1000000 },
       viaIR: true
